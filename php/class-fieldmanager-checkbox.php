ef9049733bd014c1f57a5788a8714546d89bee14
bcampeau <brad@alleyinteractive.com>
2012-10-29 15:52:01 -0400

    Added textarea type, fixed checkbox to properly check when set

--- php/class-fieldmanager-checkbox.php
+++ php/class-fieldmanager-checkbox.php
@@ -6,9 +6,10 @@ class Fieldmanager_Checkbox extends Fieldmanager_Field {
 
 	public function form_element( $value = NULL ) {
 		return sprintf(
-			'<input class="fm-element" type="checkbox" name="%s" %s />',
+			'<input class="fm-element" type="checkbox" name="%s" %s %s/>',
 			$this->name,
-			$this->get_element_attributes()
+			$this->get_element_attributes(),
+			( $value == 'o' ) ? "checked" : ""
 		);
 	}
 
