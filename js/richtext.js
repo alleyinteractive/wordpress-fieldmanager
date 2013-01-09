376f303d3bdfcce08a2966e33982943680fff9ea
Austin Smith <austin@Austins-MacBook-Air.local>
2013-01-09 11:04:10 -0500

    adding richtext js

---
+++ js/richtext.js
@@ -0,0 +1,31 @@
+( function( $ ) {
+
+tinyMCE.init( {
+	mode: "exact",
+	theme: "advanced",
+	language: "en", 
+	skin: "wp_theme",
+	editor_css: "/wp-includes/css/editor.css", 
+	theme_advanced_toolbar_align:"left",
+	theme_advanced_statusbar_location:"bottom",
+	theme_advanced_resizing:true,
+	theme_advanced_resize_horizontal:false,
+	dialog_type:"modal",
+	content_css: "/wp-content/themes/vip/kff/inc/fieldmanager/css/fieldmanager-richtext-content.css",
+	theme_advanced_toolbar_location: "top",
+	theme_advanced_buttons1: "bold,italic,strikethrough,|,bullist,numlist,blockquote,|,justifyleft,justifycenter,justifyright,|,link,unlink,|,wp_fullscreen,|,formatselect,underline,justifyfull,forecolor,|,pastetext,pasteword,removeformat,|,charmap,|,outdent,indent,|,undo,redo,|,wp_page,",
+	theme_advanced_buttons2: "",
+	plugins: "inlinepopups,spellchecker,tabfocus,paste,fullscreen,wordpress,wpeditimage,wpgallery,wplink,wpdialogs,-table,wpfullscreen",
+	style_formats:[{"title":"Custom Paragraph Style","selector":"p","classes":"kff-style1"}],
+	height: "250",
+	width: "100%",
+} );
+tinyMCE.ScriptLoader.markDone( "http://kff.dev/wp-includes/js/tinymce/langs/en.js" );
+tinyMCE.ScriptLoader.markDone( "http://kff.dev/wp-includes/js/tinymce/themes/advanced/langs/en.js" );
+
+$( document ).on( "click", '.fm-richtext', function() {
+	$this = $( this );
+	tinyMCE.execCommand( "mceAddControl", false, $this.attr( 'id' ) );
+} );
+
+} ) ( jQuery );
