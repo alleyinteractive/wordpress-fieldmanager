b17fcdf4a7d69ec64cb2ea2e9e6f6c9295fdcfe1
Austin Smith <austin@alleyinteractive.com>
2013-05-10 11:57:11 -0400

    context page

--- php/context/class-fieldmanager-context-page.php
+++ php/context/class-fieldmanager-context-page.php
@@ -4,7 +4,7 @@
  */
  
 /**
- * Use fieldmanager on the public-facing form
+ * Use fieldmanager on the public-facing theme.
  * @package Fieldmanager_Datasource
  */
 class Fieldmanager_Context_Page extends Fieldmanager_Context {
@@ -13,26 +13,35 @@ class Fieldmanager_Context_Page extends Fieldmanager_Context {
 
 	private $uniqid;
 
-	public $load_callback = Null;
+	public $did_save = False;
 
-	public $save_callback = Null;
-
+	/**
+	 * Create page context handler.
+	 */
 	public function __construct( $uniqid, $fm ) {
 		$this->fm = $fm;
 
-		$this->load_callback = function() { return array(); };
-		$this->save_callback = function() { return array(); };
-		
 		self::$forms[$uniqid] = $this;
 		$this->uniqid = $uniqid;
-		// since this only ever gets set up in init, check for submit now
-		if ( !empty( $_POST ) && !empty( $_POST['fieldmanager_page'] ) && esc_html( $_POST['fm-page-action'] ) == $uniqid ) {
+		// since this should be set up in init, check for submit now
+		if ( !empty( $_POST ) && !empty( $_POST['fm-page-action'] ) && esc_html( $_POST['fm-page-action'] ) == $uniqid ) {
 			$this->save_page_form();
 		}
 	}
 
 	public function save_page_form() {
-
+		if( !wp_verify_nonce( $_POST['fieldmanager-' . $this->fm->name . '-nonce'], 'fieldmanager-save-' . $this->fm->name ) ) {
+			$this->fm->_unauthorized_access( 'Nonce validation failed' );
+		}
+		$this->fm->data_id = $user_id;
+		if ( empty( $this->fm->data_type ) ) $this->fm->data_type = 'page';
+		if ( empty( $this->fm->data_id ) ) $this->fm->data_id = $this->uniqid;
+		$current = apply_filters( 'fm_' . $this->uniqid . '_load', array(), $this->fm );
+		$data = apply_filters( 'fm_' . $this->uniqid . '_presave', $_POST[ $this->fm->name ], $this->fm );
+		$data = $this->fm->presave_all( $data, $current );
+		$data = apply_filters( 'fm_presave_data', $data, $this->fm );
+		do_action( 'fm_' . $this->uniqid . '_save', $data, $current, $this->fm );
+		$this->did_save = True;
 	}
 
 	public static function get_form( $uniqid ) {
@@ -40,12 +49,12 @@ class Fieldmanager_Context_Page extends Fieldmanager_Context {
 	}
 
 	public function render_page_form() {
-		$values = call_user_func( $this->load_callback );
+		$current = apply_filters( 'fm_' . $this->uniqid . '_load', array(), $this->fm );
 		echo '<form method="POST">';
 		echo '<div class="fm-page-form-wrapper">';
 		printf( '<input type="hidden" name="fm-page-action" value="%s" />', sanitize_title( $this->uniqid ) );
 		wp_nonce_field( 'fieldmanager-save-' . $this->fm->name, 'fieldmanager-' . $this->fm->name . '-nonce' );
-		echo $this->fm->element_markup( $values );
+		echo $this->fm->element_markup( $current );
 		echo '</div>';
 		printf( '<input type="submit" name="fm-submit" class="button-primary" value="%s" />', $this->fm->submit_button_label ?: __( 'Save Options' ) );
 		echo '</form>';
@@ -56,4 +65,8 @@ class Fieldmanager_Context_Page extends Fieldmanager_Context {
 
 function fm_the_page_form( $uniqid ) {
 	Fieldmanager_Context_Page::get_form( $uniqid )->render_page_form();
+}
+
+function fm_page_form_did_save( $uniqid ) {
+	return Fieldmanager_Context_Page::get_form( $uniqid )->did_save;
 }
