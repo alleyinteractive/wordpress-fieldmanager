b241b38a4dab69157875e04b4699e2ff6cac3216
Matt Boynes <mboynes@alleyinteractive.com>
2013-12-11 15:56:36 -0500

    Fixing am/pm dropdowns, which didn't display properly

--- templates/datepicker.php
+++ templates/datepicker.php
@@ -18,8 +18,8 @@
 		<input class="fm-element fm-datepicker-time" type="text" value="<?php echo $this->get_minute( $value ); ?>" name="<?php echo $this->get_form_name( '[minute]' ); ?>" />
 		<?php if ( $this->use_am_pm ): ?>
 			<select name="<?php echo $this->get_form_name( '[ampm]' ); ?>" />
-				<option value="am">A.M.</option>
-				<option value="pm">P.M.</option>
+				<option value="am"<?php selected( $this->get_am_pm( $value ), 'am' ) ?>>A.M.</option>
+				<option value="pm"<?php selected( $this->get_am_pm( $value ), 'pm' ) ?>>P.M.</option>
 			</select>
 		<?php endif; ?>
 	</span>
