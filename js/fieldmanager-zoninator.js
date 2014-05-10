d399decba3c0a697fdd5083c4a1d7482f6de9275
Matt Johnson <matt@alleyinteractive.com>
2014-05-10 15:59:39 -0400

    improved JS to support adding event triggers on a post being added, and preventing multiple clicks of the submit button

--- js/fieldmanager-zoninator.js
+++ js/fieldmanager-zoninator.js
@@ -3,17 +3,28 @@
 	$( document ).ready( function() {
 
 		$('.fm-zoninator-post-form').on('submit', function(e) {
-			e.preventDefault();
-			var $button = $(this).find('.button-primary');
-			var $message = $(this).find('.fm-zone-post-form-message');
-			var label = $button.val();
-			$button.addClass('disabled').val(fm_zoninator_localization['updating']);
-			$.post(ajaxurl, { action: 'fm_zoninator_post_form_process', data: $(this).serialize() }, function(data) {
-				$button.removeClass('disabled').val(label);
-				$message.html(fm_zoninator_localization['updated']);
+			fm_zoninator_ajax(e, this);
+		});
+
+		$('.zone-posts-wrapper').on('loading.end', function() {
+			$('.fm-zoninator-post-form').on('submit', function(e) {
+				fm_zoninator_ajax(e, this);
 			});
 		});
 
 	} );
+
+	function fm_zoninator_ajax(e, form) {
+		e.preventDefault();
+		var $button = $(form).find('.button-primary');
+		var $message = $(form).find('.fm-zone-post-form-message');
+		console.log(fm_zoninator_localization);
+		var label = $button.val();
+		$button.addClass('disabled').prop('disabled', true).val(fm_zoninator_localization['updating']);
+		$.post(ajaxurl, { action: 'fm_zoninator_post_form_process', data: $(form).serialize() }, function(data) {
+			$button.removeClass('disabled').prop('disabled', false).val(label);
+			$message.html(fm_zoninator_localization['updated']);
+		});
+	}
 
 } )( jQuery );
