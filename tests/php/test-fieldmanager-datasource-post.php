d400bdb7bd5f38448d8f48e29bb24677796e721c
Daniel Bachhuber <d@danielbachhuber.com>
2014-07-22 07:22:49 -0700

    Test to indicate a use-case that's currently broken for me

--- tests/php/test-fieldmanager-datasource-post.php
+++ tests/php/test-fieldmanager-datasource-post.php
@@ -146,4 +146,43 @@ class Test_Fieldmanager_Datasource_Post extends WP_UnitTestCase {
 		$saved_value = get_post_meta( $this->parent_post->ID, 'test_save_multiple_select_with_datasource3', true );
 		$this->assertEquals( $test_data, $saved_value );
 	}
+
+	/**
+	 * Test building and saving a nested group
+	 */
+	public function test_saving_nested_groups() {
+
+		$meta_group = new \Fieldmanager_Group( '', array(
+			'name'        => 'distribution',
+			'tabbed'      => true,
+			) );
+
+		$social_group = new \Fieldmanager_Group( 'Social', array(
+			'name'        => 'social',
+			) );
+		$social_group->add_child( new \Fieldmanager_Group( 'Twitter', array(
+			'name'                    => 'twitter',
+			'children'                => array(
+				'share_text'          => new \Fieldmanager_TextArea( 'Sharing Text', array(
+					'description'     => 'What text would you like the user to include in their tweet? (Defaults to title)',
+					'attributes'      => array(
+						'style'           => 'width:100%',
+						)
+					) )
+				),
+			) ) );
+
+		$meta_group->add_child( $social_group );
+		$meta_group->add_meta_box( 'Distribution', array( 'post' ) );
+
+		$meta_group->presave( array(
+				'social'         => array(
+					'twitter'    => array(
+						'share_text'      => 'This is my sample share text'
+						),
+					),
+			) );
+
+
+	}
 }
