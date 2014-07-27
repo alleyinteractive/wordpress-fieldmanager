6f14625293a11462a58a401c2e266d1314471319
Daniel Bachhuber <d@danielbachhuber.com>
2014-07-26 21:32:41 -0400

    Create a modified gallery flow for our UX

--- js/media/fieldmanager-media.js
+++ js/media/fieldmanager-media.js
@@ -21,6 +21,43 @@ $( document ).on( 'click', '.fm-media-button', function( event ) {
 		return;
 	}
 
+	// Modification to gallery editing workflow
+	var mediaFrame = wp.media.view.MediaFrame.Post.extend({
+
+		createStates: function() {
+			var options = this.options;
+
+			this.states.add([
+
+				new media.controller.Library({
+					id:         'gallery',
+					title:      options.title,
+					priority:   40,
+					toolbar:    'main-gallery',
+					filterable: 'uploaded',
+					multiple:   'add',
+					editable:   false,
+
+					library:  media.query( _.defaults({
+						type: 'image'
+					}, options.library ) )
+				}),
+
+				new media.controller.GalleryEdit({
+					library: options.selection,
+					editing: options.editing,
+					menu:    'gallery'
+				}),
+
+				new media.controller.GalleryAdd({
+				})
+
+			]);
+
+		}
+
+	});
+
 	var media_args = {
 		// Set the title of the modal.
 		title: $el.data('choose'),
@@ -33,53 +70,58 @@ $( document ).on( 'click', '.fm-media-button', function( event ) {
 	};
 
 	if ( $el.data( 'collection' ) ) {
-		media_args.frame = 'post';
 		media_args.state = 'gallery';
+		fm_media_frame[ $el.attr('id') ] = new mediaFrame( media_args );
+	} else {
+		fm_media_frame[ $el.attr('id') ] = wp.media( media_args );
 	}
 
-	fm_media_frame[ $el.attr('id') ] = wp.media( media_args );
+	/**
+	 * Handle the selection of one or more images
+	 */
+	var mediaFrameHandleSelect = function( attachments ) {
+
+		// Normal selection doesn't pass us a collection
+		if ( ! $el.data('collection') ) {
+			attachments = fm_media_frame[ $el.attr('id') ].state().get('selection');
+		}
+
+		var ids = [],
+			preview = '';
+
+		attachments.each( function( attachment ){
+			attributes = attachment.attributes;
+			ids.push( attachment.id );
+
+			props = { size: fm_preview_size[ $el.attr('id') ] || 'thumbnail' };
+			props = wp.media.string.props( props, attributes );
+			props.align = 'none';
+			props.link = 'custom';
+			props.linkUrl = '#';
+			if ( attributes.type == 'image' ) {
+				props.url = props.src;
+				preview += 'Uploaded file:<br />';
+				preview += wp.media.string.image( props );
+			} else {
+				preview += 'Uploaded file:&nbsp;';
+				preview += wp.media.string.link( props );
+			}
+			preview += '<br /><a class="fm-media-remove fm-delete" href="#">remove</a><br />';
+
+		});
+
+		// Store for saving
+		$el.parent().find('.fm-media-id').val( ids.join(',') );
+
+		var $wrapper = $el.parent().find( '.media-wrapper' );
+		$wrapper.html( preview ).trigger( 'fieldmanager_media_preview', [ $wrapper, attachments, wp ] );
+	};
 
 	// When an image is selected, run a callback.
-	fm_media_frame[ $el.attr('id') ].on( 'select', function() {
-		// Grab the selected attachment.
-		var attachment = fm_media_frame[ $el.attr('id') ].state().get('selection').first().attributes;
-		props = { size: fm_preview_size[ $el.attr('id') ] || 'thumbnail' };
-		props = wp.media.string.props( props, attachment );
-		props.align = 'none';
-		props.link = 'custom';
-		props.linkUrl = '#';
-		$el.parent().find('.fm-media-id').val( attachment.id );
-		if ( attachment.type == 'image' ) {
-			props.url = props.src;
-			var preview = 'Uploaded file:<br />';
-			preview += wp.media.string.image( props );
-		} else {
-			var preview = 'Uploaded file:&nbsp;';
-			preview += wp.media.string.link( props );
-		}
-		preview += '<br /><a class="fm-media-remove fm-delete" href="#">remove</a>';
-		var $wrapper = $el.parent().find( '.media-wrapper' );
-		$wrapper.html( preview ).trigger( 'fieldmanager_media_preview', [ $wrapper, attachment, wp ] );
-	});
+	fm_media_frame[ $el.attr('id') ].on( 'select', mediaFrameHandleSelect );
+	fm_media_frame[ $el.attr('id') ].on( 'update', mediaFrameHandleSelect );
 
 	fm_media_frame[ $el.attr('id') ].open();
-
-	if ( $el.data('collection') ) {
-
-		var hideNonGalleryMenus = function(){
-			_.defer(function(){
-				fm_media_frame[ $el.attr('id') ].states.each( function( state ){
-					if ( 'gallery' !== state.get( 'id' )
-						&& 'gallery-edit' !== state.get( 'id' )
-						&& 'gallery-cancel' !== state.get( 'id' ) ) {
-						fm_media_frame[ $el.attr('id') ].menuItemVisibility( state.get( 'id' ), 'hide' );
-					}
-				});
-			});
-		};
-		hideNonGalleryMenus();
-		fm_media_frame[ $el.attr('id') ].on( 'menu:render', hideNonGalleryMenus );
-	}
 
 } );
 
