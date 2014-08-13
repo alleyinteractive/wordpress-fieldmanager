2e56ce759ff96857a743506bff8f9397ec1470f6
Daniel Bachhuber <d@danielbachhuber.com>
2014-08-13 09:42:35 -0700

    Modernize Group Tabs JS

--- js/fieldmanager-group-tabs.js
+++ js/fieldmanager-group-tabs.js
@@ -1,63 +1,96 @@
+var FieldmanagerGroupTabs;
 ( function( $ ) {
 
-var fm_select_tab = function( $element ) {
-	var t = $element.attr( 'href' );
-	$element.parents('.fm-tab').addClass( 'wp-tab-active' ).siblings( 'li' ).removeClass( 'wp-tab-active' );
-	$( t ).siblings( 'div' ).hide();
-	$( t ).show().trigger( 'fm_activate_tab' );
-}
+	FieldmanagerGroupTabs = {
 
-$( document ).ready( function () {
-	$( '.fm-tab-bar a' ).live( 'click', function( e ) {
-		fm_select_tab( $(this) );
-		return false;
-	} );
-	$( '.fm-tab-bar li' ).live( 'click', function( e ) {
-		fm_select_tab( $(this).children('a') );
-		return false;
-	} );
-	$('.fm-has-submenu').hoverIntent({
-		over: function(e){
+		/**
+		 * Initialize group tabs
+		 */
+		init: function() {
 
-			var b, h, o, f, m = $(this).find('.fm-submenu'), menutop, wintop, maxtop;
+			this.bindEvents();
 
-			if ( m.is(':visible') )
-				return;
-
-			menutop = $(this).offset().top;
-			menuleft = $(this).position().left;
-			menuwidth = $(this).width();
-			wintop = $(window).scrollTop();
-			maxtop = menutop - wintop - 30; // max = make the top of the sub almost touch admin bar
-
-			b = menutop + m.height() + 1; // Bottom offset of the menu
-			h = $('#wpwrap').height(); // Height of the entire page
-			o = 60 + b - h;
-			f = $(window).height() + wintop - 15; // The fold
-
-			if ( f < (b - o) )
-				o = b - f;
-
-			if ( o > maxtop )
-				o = maxtop;
-
-			if ( o > 1 )
-				m.css('margin-top', '-'+o+'px');
-			else
-				m.css('margin-top', '');
-
-			$(this).find('.fm-submenu').css('left', menuleft);
-
-			$(this).find('.fm-submenu').removeClass('sub-open');
-			m.addClass('sub-open');
 		},
-		out: function(){
-			$(this).find('.fm-submenu').removeClass('sub-open').css('margin-top', '');
+
+		/**
+		 * Bind events
+		 */
+		bindEvents: function() {
+
+			$( '.fm-tab-bar a' ).on( 'click', $.proxy( function( e ) {
+				e.preventDefault();
+				this.selectTab( $( e.currentTarget ) );
+			}, this ) );
+			$( '.fm-tab-bar li' ).on( 'click', $.proxy( function( e ) {
+				e.preventDefault();
+				this.selectTab( $( e.currentTarget ).children('a') );
+			}, this ) );
+
+			$('.fm-has-submenu').hoverIntent({
+				over: function(e){
+
+					var el = $(this);
+
+					var b, h, o, f, m = el.find('.fm-submenu'), menutop, wintop, maxtop;
+
+					if ( m.is(':visible') )
+						return;
+
+					menutop = el.offset().top;
+					menuleft = el.position().left;
+					menuwidth = el.width();
+					wintop = $(window).scrollTop();
+					maxtop = menutop - wintop - 30; // max = make the top of the sub almost touch admin bar
+
+					b = menutop + m.height() + 1; // Bottom offset of the menu
+					h = $('#wpwrap').height(); // Height of the entire page
+					o = 60 + b - h;
+					f = $(window).height() + wintop - 15; // The fold
+
+					if ( f < (b - o) )
+						o = b - f;
+
+					if ( o > maxtop )
+						o = maxtop;
+
+					if ( o > 1 )
+						m.css('margin-top', '-'+o+'px');
+					else
+						m.css('margin-top', '');
+
+					el.find('.fm-submenu').css('left', menuleft);
+
+					el.find('.fm-submenu').removeClass('sub-open');
+					m.addClass('sub-open');
+				},
+				out: function(){
+					$(this).find('.fm-submenu').removeClass('sub-open').css('margin-top', '');
+				},
+				timeout: 200,
+				sensitivity: 7,
+				interval: 90
+			});
+
 		},
-		timeout: 200,
-		sensitivity: 7,
-		interval: 90
+
+		/**
+		 * Select a given tab
+		 */
+		selectTab: function( $element ) {
+
+			var t = $element.attr( 'href' );
+			$element.parents('.fm-tab').addClass( 'wp-tab-active' ).siblings( 'li' ).removeClass( 'wp-tab-active' );
+			$( t ).siblings( 'div' ).hide();
+			$( t ).show().trigger( 'fm_activate_tab' );
+
+		}
+
+	};
+
+	$(document).ready( function(){
+
+		FieldmanagerGroupTabs.init();
+
 	});
-} );
 
 } )( jQuery );
