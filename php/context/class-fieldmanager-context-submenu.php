8375c64f8c086d83ef330f9f15f1b86876849f47
David Herrera <david@alleyinteractive.com>
2014-09-05 13:51:55 -0400

    Redirect to the correct admin URL after saving

--- php/context/class-fieldmanager-context-submenu.php
+++ php/context/class-fieldmanager-context-submenu.php
@@ -140,7 +140,8 @@ class Fieldmanager_Context_Submenu extends Fieldmanager_Context {
 	public function handle_submenu_save() {
 		if ( ! empty( $_POST ) && ! empty( $_GET['page'] ) && $_GET['page'] == $this->menu_slug && current_user_can( $this->capability ) ) {
 			if ( $this->save_submenu_data() ) {
-				wp_redirect( add_query_arg( array( 'page' => $this->menu_slug, 'msg' => 'success' ), admin_url( $this->parent_slug ) ) );
+				$url = $this->is_network_submenu ? network_admin_url( $this->parent_slug ) : admin_url( $this->parent_slug );
+				wp_redirect( add_query_arg( array( 'page' => $this->menu_slug, 'msg' => 'success' ), esc_url_raw( $url ) ) );
 				exit;
 			}
 		}
