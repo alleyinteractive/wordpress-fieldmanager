eba8eb0a932e801dfcbc44c3044a45719c315e70
Matt Boynes <mboynes@alleyinteractive.com>
2014-10-09 17:26:11 -0400

    Initial work on replacing FM's richtextarea integration with wp_editor

--- js/richtext.js
+++ js/richtext.js
@@ -2,14 +2,66 @@
 
 fm.richtextarea = {
 	add_rte_to_visible_textareas: function() {
-		$( '.fm-richtext:visible' ).each( function() {
-			if ( !$( this ).hasClass( 'fm-tinymce' ) ) {
-				var opts = $( this ).data( 'mce-options' );
-				if ( opts ) {
-					opts['elements'] = $( this ).attr( 'id' );
-					tinyMCE.init( opts );
-					$( this ).addClass( 'fm-tinymce' );
+		$( 'textarea.fm-richtext:visible' ).each( function() {
+			if ( ! $( this ).hasClass( 'fm-tinymce' ) ) {
+				var init, ed_id, mce_options, qt_options, proto_id;
+				ed_id = $( this ).attr( 'id' );
+				proto_id = $( this ).data( 'proto-id' );
+
+				// Clean up the proto id which appears in some of the wp_editor generated HTML
+				$( this ).closest( '.fm-wrapper' ).html( $( this ).closest( '.fm-wrapper' ).html().replace( new RegExp( proto_id, 'g' ), ed_id ) );
+
+				if ( typeof tinymce !== 'undefined' ) {
+
+					if ( typeof tinyMCEPreInit.mceInit[ ed_id ] === 'undefined' ) {
+						// This needs to be initialized, so we need to get the options from the proto
+						if ( proto_id && typeof tinyMCEPreInit.mceInit[ proto_id ] !== 'undefined' ) {
+							mce_options = tinyMCEPreInit.mceInit[ proto_id ];
+							mce_options.body_class = mce_options.body_class.replace( proto_id, ed_id );
+							mce_options.selector = mce_options.selector.replace( proto_id, ed_id );
+							mce_options.wp_skip_init = false;
+							tinyMCEPreInit.mceInit[ ed_id ] = mce_options;
+						} else {
+							// TODO: No data to work with, this should throw some sort of error
+							return;
+						}
+
+						if ( proto_id && typeof tinyMCEPreInit.qtInit[ proto_id ] !== 'undefined' ) {
+							qt_options = tinyMCEPreInit.qtInit[ proto_id ];
+							qt_options.id = qt_options.id.replace( proto_id, ed_id );
+							tinyMCEPreInit.qtInit[ ed_id ] = qt_options;
+						}
+
+						init = tinyMCEPreInit.mceInit[ ed_id ];
+
+						try {
+							result = tinymce.init( init );
+
+							if ( ! window.wpActiveEditor ) {
+								window.wpActiveEditor = ed_id;
+							}
+						} catch(e){}
+
+						try {
+							if ( typeof tinyMCEPreInit.qtInit[ ed_id ] !== 'undefined' ) {
+								quicktags( tinyMCEPreInit.qtInit[ ed_id ] );
+
+								if ( ! window.wpActiveEditor ) {
+									window.wpActiveEditor = ed_id;
+								}
+							}
+						} catch(e){};
+					}
 				}
+
+				$('.wp-editor-wrap').on( 'click.wp-editor', function() {
+					if ( this.id ) {
+						window.wpActiveEditor = this.id.slice( 3, -5 );
+					}
+				});
+
+				$( this ).addClass( 'fm-tinymce' );
+
 			}
 		} );
 	},
@@ -32,7 +84,6 @@ fm.richtextarea = {
 		});
 	}
 }
-$( document ).ready( fm.richtextarea.add_rte_to_visible_textareas );
 $( document ).on( 'fm_collapsible_toggle fm_added_element fm_activate_tab fm_displayif_toggle', fm.richtextarea.add_rte_to_visible_textareas );
 $( document ).on( 'fm_sortable_drag', fm.richtextarea.drag_rte_disable_control );
 $( document ).on( 'fm_sortable_drop', fm.richtextarea.drop_rte_enable_control );
--- php/class-fieldmanager-richtextarea.php
+++ php/class-fieldmanager-richtextarea.php
@@ -75,193 +75,52 @@ class Fieldmanager_RichTextArea extends Fieldmanager_Field {
 
 
 	/**
-	 * Construct default attributes; 50x10 textarea
+	 * Construct defaults for this field attributes.
+	 *
 	 * @param array $options
 	 * @todo dectect locale and apply correct language file
 	 */
 	public function __construct( $label, $options = array() ) {
-		if ( ! isset( self::$tinymce_major_version ) ) {
-			global $tinymce_version;
-			self::$tinymce_major_version = intval( substr( $tinymce_version, 0, 1 ) );
-		}
-
-		if ( !self::$has_registered_tinymce ) {
-			if ( 4 > self::$tinymce_major_version ) {
-				wp_enqueue_script( 'tiny_mce.js', includes_url( 'js/tinymce/tiny_mce.js' ) );
-				wp_enqueue_script( 'wp-langs-en.js', includes_url( 'js/tinymce/langs/wp-langs-en.js' ) );
-			} else {
-				wp_enqueue_script( 'tiny_mce.js', includes_url( 'js/tinymce/tinymce.min.js' ) );
-				wp_enqueue_style( 'editor-buttons' );
-			}
-
-			self::$has_registered_tinymce = True;
-			// add tinyMCEPreInit early enough in the process that it's available before TinyMCE inits.
-			// WP Core may overwrite tinyMCEPreInit, but that's fine with us.
-			add_action( 'admin_print_scripts', function() {
-				printf( '<script>
-if ( "undefined" === typeof tinyMCEPreInit ) tinyMCEPreInit = { base: "%s", suffix: "" };
-</script>',
-					includes_url( 'js/tinymce' )
-				);
-			} );
-
-			if ( 4 > self::$tinymce_major_version ) {
-				// Mark the language files as loaded; prevents a baffling JS error.
-				add_action( 'admin_head', function() {
-					printf(
-						'<script type="text/javascript">
-	tinyMCE.ScriptLoader.markDone( "%1$sjs/tinymce/langs/en.js" );
-	tinyMCE.ScriptLoader.markDone( "%1$sjs/tinymce/themes/advanced/langs/en.js" );
-	</script>',
-						includes_url()
-					);
-				} );
-			}
-		}
-		$this->attributes = array(
-			'cols' => '50',
-			'rows' => '10'
-		);
 		$this->sanitize = function( $value ) {
-			return wpautop( wp_filter_post_kses( $value ) ); // run through wpautop first to preserve breaks.
+			return wp_filter_post_kses( wpautop( $value ) ); // run through wpautop first to preserve breaks.
 		};
-		// Unlike WP Core, we init TinyMCE on demand, and preserve its natural ability to move
-		// about the DOM—richtext.js takes care of initializing our options, which are stored
-		// per-field, not globally.
-		fm_add_script( 'fm_richtext', 'js/richtext.js', array( 'jquery' ), '1.0.2' );
+		fm_add_script( 'fm_richtext', 'js/richtext.js', array( 'jquery' ), '1.0.3' );
 		parent::__construct( $label, $options );
 	}
 
 
 	/**
-	 * Enqueue JS for TinyMCE control.
-	 * We push out all TinyMCE scripts, because we might have many controls on the page
-	 * and would otherwise have to compare them. Our default editor ships with all these
-	 * controls, anyways.
-	 */
-	public static function enqueue_scripts() {
-		wp_enqueue_script( 'wplink' );
-		wp_enqueue_script( 'wpeditor' );
-		wp_enqueue_script( 'wpdialogs-popup' );
-		wp_enqueue_style( 'wp-jquery-ui-dialog' );
-		wp_enqueue_script( 'wp-fullscreen' );
-		add_action( 'admin_print_scripts', function() {
-			$post = get_post();
-			$args = array();
-			if ( $post->ID ) {
-				$args['post'] = $post->ID;
-			}
-			wp_enqueue_media( $args ); // generally on post pages this will not have an impact.
-		} );
-	}
-
-	/**
-	 * Cribbed from _WP_Editors, render the necessary HTML at the bottom of the page to show
-	 * the link and fullscreen edit popups
-	 * @return void
-	 */
-	public static function editor_js() {
-		if ( ! class_exists( '_WP_Editors' ) ) {
-			require( ABSPATH . WPINC . '/class-wp-editor.php' );
-
-			_WP_Editors::wp_link_dialog();
-			_WP_Editors::wp_fullscreen_html();
-		}
-	}
-
-	/**
-	 * Get default TinyMCE options; can be overriden per field type with $this->init_options
-	 * @return array of options to pass to TinyMCE in JS.
-	 */
-	public function get_mce_options() {
-		$editor_id = $this->get_element_id();
-		$buttons = array(
-			implode( ',', apply_filters( 'mce_buttons', $this->buttons_1, $editor_id ) ),
-			implode( ',', apply_filters( 'mce_buttons_2', $this->buttons_2, $editor_id ) ),
-			implode( ',', apply_filters( 'mce_buttons_3', $this->buttons_3, $editor_id ) ),
-			implode( ',', apply_filters( 'mce_buttons_4', $this->buttons_4, $editor_id ) ),
-		);
-
-		$plugins = array( 'tabfocus', 'paste', 'media', 'fullscreen', 'wordpress', 'wpeditimage', 'wpgallery', 'wplink', 'wpdialogs' );
-		$options = array(
-			'mode' => 'exact',
-			'language' => 'en',
-			'editor_css' => '/wp-includes/css/editor.css',
-			'content_css' => apply_filters( 'mce_css', fieldmanager_get_baseurl() . 'css/fieldmanager-richtext-content.css' ),
-			'height' => '250',
-			'width' => '100%',
-			'convert_urls' => false,
-		);
-
-		if ( 4 > self::$tinymce_major_version ) {
-			$options = array_merge( $options, array(
-				'theme' => 'advanced',
-				'skin' => 'wp_theme',
-				'theme_advanced_buttons1' => $buttons[0],
-				'theme_advanced_buttons2' => $buttons[1],
-				'theme_advanced_buttons3' => $buttons[2],
-				'theme_advanced_buttons4' => $buttons[3],
-				'theme_advanced_toolbar_align' => 'left',
-				'theme_advanced_statusbar_location' => 'bottom',
-				'theme_advanced_resizing' => true,
-				'theme_advanced_resize_horizontal' => false,
-				'theme_advanced_toolbar_location' => 'top',
-				'dialog_type' => 'modal',
-			) );
-			$plugins[] = 'inlinepopups';
-			$plugins[] = 'spellchecker';
-		} else {
-			$options = array_merge( $options, array(
-				'theme' => 'modern',
-				'menubar' => false,
-				'toolbar1' => $buttons[0],
-				'toolbar2' => $buttons[1],
-				'toolbar3' => $buttons[2],
-				'toolbar4' => $buttons[3],
-			) );
-		}
-
-		$options['plugins'] = implode( ',', array_unique( apply_filters('tiny_mce_plugins', $plugins ) ) );
-		$options = array_merge( $options, $this->init_options );
-		if ( $this->apply_mce_filters ) {
-			$options['content_css'] = apply_filters( 'mce_css', $options['content_css'] );
-			$options = apply_filters( 'tiny_mce_before_init', $options, $editor_id );
-		}
-		if ( isset( $options['style_formats'] ) && !is_array( $options['style_formats'] ) ) {
-			$options['style_formats'] = json_decode( $options['style_formats'] );
-		}
-		if ( $this->add_code_plugin ) {
-			$options['external_plugins']['code'] = fieldmanager_get_baseurl() . '/js/tinymce.code.js';
-		}
-
-		unset( $options['elements'] );
-		return $options;
-	}
-
-	/**
 	 * Render the form element, which is a textarea by default.
+	 *
 	 * @param mixed $value
 	 * @return string HTML
 	 */
 	public function form_element( $value = '' ) {
-		if ( !self::$has_added_footer_scripts ) {
-			if ( is_admin() ) {
-				add_action( 'admin_print_footer_scripts', array( __CLASS__, 'editor_js'), 50 );
-				add_action( 'admin_footer', array( __CLASS__, 'enqueue_scripts'), 1 );
-			} else {
-				add_action( 'wp_print_footer_scripts', array( __CLASS__, 'editor_js'), 50 );
-				add_action( 'wp_footer', array( __CLASS__, 'enqueue_scripts'), 1 );
-			}
-			self::$has_added_footer_scripts = True;
-		}
-		return sprintf(
-			'<textarea class="fm-element fm-richtext" name="%1$s" id="%2$s" %3$s data-mce-options="%5$s">%4$s</textarea>',
-			esc_attr( $this->get_form_name() ),
-			esc_attr( $this->get_element_id() ),
-			$this->get_element_attributes(),
-			esc_textarea( $value ),
-			esc_attr( json_encode( $this->get_mce_options() ), ENT_QUOTES )
+		ob_start();
+		$proto = $this->has_proto();
+		$settings = array(
+			'textarea_name' => $this->get_form_name(),
+			'editor_class' => 'fm-element fm-richtext',
+			'tinymce' => array( 'wp_skip_init' => false ),
 		);
+		if ( $proto ) {
+			add_filter( 'the_editor', array( $this, 'add_proto_id' ) );
+			$settings['tinymce'] = array( 'wp_skip_init' => true );
+		}
+		wp_editor( $value, $this->get_element_id(), $settings );
+		if ( $proto ) {
+			remove_filter( 'the_editor', array( $this, 'add_proto_id' ) );
+		}
+		return ob_get_clean();
 	}
 
+	/**
+	 * Note the ID for the proto so in repeated fields we can dig up the editor settings.
+	 *
+	 * @param string $editor HTML for the editor.
+	 * @return string The editor HTML with a `data-proto-id` attribute added.
+	 */
+	public function add_proto_id( $editor ) {
+		return str_replace( '<textarea', '<textarea data-proto-id="' . $this->get_element_id() . '"', $editor );
+	}
 }
