1d9f960ef7abd3109016ade2867dc688c4b7b2b5
Daniel Bachhuber <d@danielbachhuber.com>
2014-10-17 15:14:40 -0700

    This is the difference between VIP and Git master

--- php/context/class-fieldmanager-context-user.php
+++ php/context/class-fieldmanager-context-user.php
@@ -20,7 +20,7 @@ class Fieldmanager_Context_User extends Fieldmanager_Context {
 	 * @param string $title
 	 * @param Fieldmanager_Field $fm
 	 */
-	public function __construct( $title = '', $fm = Null ) {
+	public function __construct( $title = '', $fm = null ) {
 		$this->title = $title;
 		$this->fm = $fm;
 		add_action( 'show_user_profile', array( $this, 'render_user_form' ) );
@@ -35,7 +35,7 @@ class Fieldmanager_Context_User extends Fieldmanager_Context {
 	 */
 	public function render_user_form( $user ) {
 		$values = get_user_meta( $user->ID, $this->fm->name );
-		$values = empty( $values ) ? Null : $values[0];
+		$values = empty( $values ) ? null : $values[0];
 		if ( !empty( $this->title ) ) echo '<h3>' . $this->title . '</h3>';
 		echo '<div class="fm-user-form-wrapper">';
 		wp_nonce_field( 'fieldmanager-save-' . $this->fm->name, 'fieldmanager-' . $this->fm->name . '-nonce' );
@@ -48,24 +48,32 @@ class Fieldmanager_Context_User extends Fieldmanager_Context {
 	}
 
 	/**
-	 * Save user form
+	 * Save user form.
+	 *
 	 * @param int $user_id
-	 * @return void
 	 */
 	public function save_user_form( $user_id ) {
-		if ( ! empty( $_POST ) && current_user_can( 'edit_user', $user_id ) ) {
+		if ( ! empty( $_POST ) && ! empty( $_POST['fieldmanager-' . $this->fm->name . '-nonce'] ) && current_user_can( 'edit_user', $user_id ) ) {
 			// Make sure that our nonce field arrived intact
-			if( !wp_verify_nonce( $_POST['fieldmanager-' . $this->fm->name . '-nonce'], 'fieldmanager-save-' . $this->fm->name ) ) {
+			if ( ! wp_verify_nonce( $_POST['fieldmanager-' . $this->fm->name . '-nonce'], 'fieldmanager-save-' . $this->fm->name ) ) {
 				$this->fm->_unauthorized_access( __( 'Nonce validation failed', 'fieldmanager' ) );
 			}
-			$this->fm->data_id = $user_id;
-			$this->fm->data_type = 'user';
-			$current = get_user_meta( $user->ID, $this->fm->name, True );
-			$value = isset( $_POST[ $this->fm->name ] ) ? $_POST[ $this->fm->name ] : "";
-			$data = $this->fm->presave_all( $value, $current );
-			$data = apply_filters( 'fm_user_presave_data', $data, $this->fm );
-			update_user_meta( $user_id, $this->fm->name, $data );
+
+			$this->save_to_user_meta( $user_id, ( isset( $_POST[ $this->fm->name ] ) ? $_POST[ $this->fm->name ] : "" ) );
 		}
 	}
 
+	/**
+	 * Save data to user meta.
+	 *
+	 * @param  int $user_id
+	 */
+	public function save_to_user_meta( $user_id, $value ) {
+		$this->fm->data_id = $user_id;
+		$this->fm->data_type = 'user';
+		$current = get_user_meta( $user_id, $this->fm->name, true );
+		$data = $this->fm->presave_all( $value, $current );
+		$data = apply_filters( 'fm_user_presave_data', $data, $this->fm );
+		update_user_meta( $user_id, $this->fm->name, $data );
+	}
 }
