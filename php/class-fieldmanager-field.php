87a94a3a11df689d9e76d94be9709012d80af63c
jitendraharpalani58 <jitendra.harpalani@fusion.net>
2014-11-04 18:15:40 +0530

    use proper names

--- php/class-fieldmanager-field.php
+++ php/class-fieldmanager-field.php
@@ -207,12 +207,12 @@ abstract class Fieldmanager_Field {
 	public $display_if = array();
 
 	/**
-	* @var string
+	* @var boolean
 	* Where the new item should to added ( top/bottom ) of the stack. Used by Add Another button
-	* $element->stack_order = 'LIFO' : Default, add at the bottom of stack
-	* $element->stack_order = 'FIFO' : Add at the top of stack
+	* $element->add_more_at_top = true : Default, add at the bottom of stack
+	* $element->add_more_at_top = false : Add at the top of stack
 	*/
-	public $stack_order = 'LIFO';
+	public $add_more_at_top = True;
 
 	/**
 	 * @var boolean
@@ -435,7 +435,7 @@ abstract class Fieldmanager_Field {
 
 		// After starting the field, apply a filter to allow other plugins to append functionality
 		$out = apply_filters( 'fm_element_markup_start', $out, $this );
-		if ( ( 0 == $this->limit || ( $this->limit > 1 && $this->limit > $this->minimum_count ) ) && 'FIFO' == $this->stack_order ) {
+		if ( ( 0 == $this->limit || ( $this->limit > 1 && $this->limit > $this->minimum_count ) ) && !$this->add_more_at_top ) {
 			$out .= $this->add_another();
 		}
 
@@ -451,7 +451,7 @@ abstract class Fieldmanager_Field {
 			}
 			$out .= $this->single_element_markup( $value );
 		}
-		if ( ( 0 == $this->limit || ( $this->limit > 1 && $this->limit > $this->minimum_count ) ) && 'LIFO' == $this->stack_order ) {
+		if ( ( 0 == $this->limit || ( $this->limit > 1 && $this->limit > $this->minimum_count ) ) && $this->add_more_at_top ) {
 			$out .= $this->add_another();
 		}
 
@@ -835,14 +835,20 @@ abstract class Fieldmanager_Field {
 		if ( empty( $this->add_more_label ) ) {
 			$this->add_more_label = 'group' == $this->field_class ? __( 'Add group', 'fieldmanager' ) : __( 'Add field', 'fieldmanager' );
 		}
+		if( $this->add_more_at_top ) {
+			$add_more_attr = "add_more_at_top";
+		} else {
+			$add_more_attr = "add_more_at_bottom";
+		}
+
 		$out = '<div class="fm-add-another-wrapper">';
 		$out .= sprintf(
-			'<input type="button" class="%s" value="%s" name="%s" data-related-element="%s" data-stack-order="%s" data-limit="%d" />',
+			'<input type="button" class="%s" value="%s" name="%s" data-related-element="%s" data-add-more-at-top="%s" data-limit="%d" />',
 			esc_attr( implode( ' ', $classes ) ),
 			esc_attr( $this->add_more_label ),
 			esc_attr( 'fm_add_another_' . $this->name ),
 			esc_attr( $this->name ),
-			esc_attr( $this->stack_order ),
+			esc_attr( $add_more_attr ),
 			intval( $this->limit )
 		);
 		$out .= '</div>';
