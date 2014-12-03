e3e0876e05f391da8142eed17418888c847aa94c
Austin Smith <austin@alleyinteractive.com>
2014-12-03 18:57:30 -0500

    unit tests

--- tests/php/test-fieldmanager-datasource-post.php
+++ tests/php/test-fieldmanager-datasource-post.php
@@ -238,6 +238,47 @@ class Test_Fieldmanager_Datasource_Post extends WP_UnitTestCase {
 	}
 
 	/**
+	 * Test save_to_post_parent logic
+	 */
+	public function test_post_parent() {
+		$test_data = $this->child_post_a->ID;
+		$children = new Fieldmanager_Autocomplete( array(
+			'name' => 'test_parent',
+			'datasource' => new Fieldmanager_Datasource_Post( array(
+				'query_args' => array(
+					'post_type' => 'post'
+				),
+				'save_to_post_parent' => true,
+			) ),
+		) );
+		$this->save_values( $children, $this->parent_post, $test_data );
+		$parent = get_post( $this->parent_post->ID );
+		$this->assertEquals( $this->parent_post->post_parent, $this->child_post_a->ID );
+		$this->assertEquals( get_post_meta( $this->parent_post->ID, 'test_parent', true ), $this->child_post_a->ID );
+	}
+
+	/**
+	 * Test save_to_post_parent_only logic
+	 */
+	public function test_post_parent() {
+		$test_data = $this->child_post_a->ID;
+		$children = new Fieldmanager_Autocomplete( array(
+			'name' => 'test_parent',
+			'datasource' => new Fieldmanager_Datasource_Post( array(
+				'query_args' => array(
+					'post_type' => 'post'
+				),
+				'save_to_post_parent' => true,
+				'save_to_post_parent_only' => true,
+			) ),
+		) );
+		$this->save_values( $children, $this->parent_post, $test_data );
+		$parent = get_post( $this->parent_post->ID );
+		$this->assertEquals( $this->parent_post->post_parent, $this->child_post_a->ID );
+		$this->assertEquals( get_post_meta( $this->parent_post->ID, 'test_parent', true ), false );
+	}
+
+	/**
 	 * Test that a user lacking permission can not add meta to a child post.
 	 *
 	 * @expectedException WPDieException
