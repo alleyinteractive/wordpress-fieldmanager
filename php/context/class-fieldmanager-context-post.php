d2e1617d9e7fc821e728623fcd7153a8178137d6
jitendraharpalani58 <jitendra.harpalani@fusion.net>
2014-12-05 15:30:42 +0530

    Merge branch 'save-to-post-parent'

--- php/context/class-fieldmanager-context-post.php
+++ php/context/class-fieldmanager-context-post.php
@@ -40,6 +40,11 @@ class Fieldmanager_Context_Post extends Fieldmanager_Context {
 	public $fm = null;
 
 	/**
+	 * @var boolean
+	 */
+	private static $doing_internal_update = false;
+
+	/**
 	 * Add a context to a fieldmanager
 	 * @param string $title
 	 * @param string|string[] $post_types
@@ -144,6 +149,9 @@ class Fieldmanager_Context_Post extends Fieldmanager_Context {
 	 * @return void
 	 */
 	public function delegate_save_post( $post_id ) {
+		if ( self::$doing_internal_update ) {
+			return;
+		}
 		if( defined( 'DOING_CRON' ) && DOING_CRON ) {
 			$this->save_fields_for_cron( $post_id );
 		} else {
@@ -210,7 +218,9 @@ class Fieldmanager_Context_Post extends Fieldmanager_Context {
 	 * @return void
 	 */
 	public function save_fields_for_cron( $post_id ) {
-		if ( ! in_array( get_post_type( $post_id ), $this->post_types ) ) return;
+		if ( ! in_array( get_post_type( $post_id ), $this->post_types ) ) {
+			return;
+		}
 		// don't save values since we aren't provided with any; just trigger presave so that subclass handlers can process as necessary
 		$this->fm->skip_save = true;
 		$current = get_post_meta( $post_id, $this->fm->name, true );
@@ -224,12 +234,24 @@ class Fieldmanager_Context_Post extends Fieldmanager_Context {
 	 * @return void
 	 */
 	public function save_to_post_meta( $post_id, $data ) {
-		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+			return;
+		}
 		$this->fm->data_id = $post_id;
 		$this->fm->data_type = 'post';
 		$current = get_post_meta( $this->fm->data_id, $this->fm->name, true );
 		$data = $this->fm->presave_all( $data, $current );
 		if ( !$this->fm->skip_save ) update_post_meta( $post_id, $this->fm->name, $data );
+	}
+
+	/**
+	 * Helper for fieldmanager internals to save a post without worrying about infinite loops
+	 */
+	public static function safe_update_post( $args ) {
+		self::$doing_internal_update = true;
+		$ret = wp_update_post( $args );
+		self::$doing_internal_update = false;
+		return $ret;
 	}
 
 }
