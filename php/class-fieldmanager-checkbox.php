984fccd9e56eb5f6b5636a0239883cf93029beb5
Matt Boynes <mboynes@alleyinteractive.com>
2015-03-02 09:10:56 -0500

    Fix an issue with checkboxes

--- php/class-fieldmanager-checkbox.php
+++ php/class-fieldmanager-checkbox.php
@@ -43,12 +43,13 @@ class Fieldmanager_Checkbox extends Fieldmanager_Field {
 	 */
 	public function form_element( $value = NULL ) {
 		return sprintf(
-			'<input class="fm-element" type="checkbox" name="%1$s" value="%2$s" %3$s %4$s id="%5$s" />',
+			'<input type="hidden" name="%1$s" value="%6$s" /><input class="fm-element" type="checkbox" name="%1$s" value="%2$s" %3$s %4$s id="%5$s" />',
 			esc_attr( $this->get_form_name() ),
 			esc_attr( (string) $this->checked_value ),
 			$this->get_element_attributes(),
 			( $value == $this->checked_value ) ? 'checked="checked"' : "",
-			esc_attr( $this->get_element_id() )
+			esc_attr( $this->get_element_id() ),
+			$this->unchecked_value
 		);
 	}
 
@@ -58,10 +59,9 @@ class Fieldmanager_Checkbox extends Fieldmanager_Field {
 	 * @return mixed proper value
 	 */
 	public function presave( $value = NULL, $current_value = array() ) {
-		if ( $value == $this->checked_value ) {
+		if ( $value == $this->checked_value || $value == $this->unchecked_value ) {
 			return $value;
-		}
-		elseif ( empty( $value ) ) {
+		} elseif ( empty( $value ) ) {
 			return $this->unchecked_value;
 		}
 		else {
