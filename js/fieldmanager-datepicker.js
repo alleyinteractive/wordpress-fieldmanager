be00ddf829400d01dd8335562aba4479bfe6c13f
David Herrera <david@alleyinteractive.com>
2015-11-22 14:55:17 -0500

    Fix Datepicker initialization in the Customizer

--- js/fieldmanager-datepicker.js
+++ js/fieldmanager-datepicker.js
@@ -11,5 +11,8 @@
 	}
 
 	$( document ).ready( fm.datepicker.add_datepicker );
-	$( document ).on( 'fm_collapsible_toggle fm_added_element fm_displayif_toggle fm_activate_tab', fm.datepicker.add_datepicker );
+	$( document ).on(
+		'fm_collapsible_toggle fm_added_element fm_displayif_toggle fm_activate_tab fm_customizer_control_section_expanded',
+		fm.datepicker.add_datepicker
+	);
 } ) ( jQuery );
