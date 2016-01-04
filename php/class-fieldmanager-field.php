e0282155a3f4d03000f654bd4f40c1ab40133c6c
Steven Slack <steven@alleyinteractive.com>
2016-01-04 10:35:07 -0500

    removed toggle indicator span

--- php/class-fieldmanager-field.php
+++ php/class-fieldmanager-field.php
@@ -939,7 +939,7 @@ abstract class Fieldmanager_Field {
 	 * @return string
 	 */
 	public function get_collapse_handle() {
-		return  sprintf( '<a href="#" class="field-group-toggler" title="%1$s"><span class="screen-reader-text">%1$s</span><span class="toggle-indicator" aria-hidden="true"></span></a>', esc_attr__( 'Toggle field group', 'fieldmanager' ) );
+		return  sprintf( '<a href="#" class="field-group-toggler" title="%1$s"><span class="screen-reader-text">%1$s</span></a>', esc_attr__( 'Toggle field group', 'fieldmanager' ) );
 	}
 
 	/**
