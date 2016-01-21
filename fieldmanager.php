0cb0468e537922d28d2ecc90519c7b774a0c82a3
Nathaniel Taintor <goldenapplesdesign@gmail.com>
2016-01-21 13:37:57 -0500

    spaces to tabs

--- fieldmanager.php
+++ fieldmanager.php
@@ -355,7 +355,7 @@ function fm_calculate_context() {
 				break;
 			// Context = "term".
 			case 'edit-tags.php':
-            case 'term.php':
+			case 'term.php':
 				if ( !empty( $_POST['taxonomy'] ) ) {
 					$calculated_context = array( 'term', sanitize_text_field( $_POST['taxonomy'] ) );
 				} elseif ( !empty( $_GET['taxonomy'] ) ) {
