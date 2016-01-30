43b320ae7b68d7de7a4fa8b8b185a23d631466dc
David Herrera <david@alleyinteractive.com>
2016-01-29 21:16:53 -0500

    Trigger an event after an FM RTE loads TinyMCE

--- js/richtext.js
+++ js/richtext.js
@@ -36,6 +36,16 @@
 
 						try {
 							if ( 'html' !== fm.richtextarea.mode_enabled( this ) ) {
+								tinyMCEPreInit.mceInit[ ed_id ].setup = function ( ed ) {
+									ed.on( 'init', function( args ) {
+										/**
+										 * Event after TinyMCE loads for an Fieldmanager_RichTextArea.
+										 *
+										 * @var {Object} TinyMCE instance.
+										 */
+										 $( document ).trigger( 'fm_richtext_init', ed );
+									});
+								};
 								tinymce.init( tinyMCEPreInit.mceInit[ ed_id ] );
 								$( this ).closest( '.wp-editor-wrap' ).on( 'click.wp-editor', function() {
 									if ( this.id ) {
