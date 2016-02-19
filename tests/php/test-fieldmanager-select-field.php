54f9d089fe5d747164b931a20e9a5df997e1f8e6
Noah Schoenholtz <noah@alleyinteractive.com>
2016-02-19 14:00:29 -0500

    fix handling for repeatable multiple select fields #352

--- tests/php/test-fieldmanager-select-field.php
+++ tests/php/test-fieldmanager-select-field.php
@@ -149,6 +149,27 @@ class Test_Fieldmanager_Select_Field extends WP_UnitTestCase {
 		);
 	}
 
+	public function test_repeatable_multiselect_save() {
+		$fm = new Fieldmanager_Select( array(
+			'name' => 'base_field',
+			'multiple' => true,
+			'limit' => 0,
+			'options' => array( 'one', 'two', 'three' ),
+		) );
+
+		$fm->add_meta_box( 'base_field', $this->post->post_type )->save_to_post_meta( $this->post->ID, array( 'two' ) );
+		$saved_value = get_post_meta( $this->post->ID, 'base_field', true );
+		$this->assertSame( array( 'two' ), $saved_value );
+
+		$fm->add_meta_box( 'base_field', $this->post->post_type )->save_to_post_meta( $this->post->ID, array( 'two', 'three' ) );
+		$saved_value = get_post_meta( $this->post->ID, 'base_field', true );
+		$this->assertSame( array( 'two', 'three' ), $saved_value );
+
+		$fm->add_meta_box( 'base_field', $this->post->post_type )->save_to_post_meta( $this->post->ID, '' );
+		$saved_value = get_post_meta( $this->post->ID, 'base_field', true );
+		$this->assertNull( $saved_value );
+	}
+
 	public function test_repeatable_render() {
 		$fm = new Fieldmanager_Select( array(
 			'name' => 'base_field',
