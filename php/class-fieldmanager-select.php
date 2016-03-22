ae70d8ae798fb1680c38b5843bceaad72de6369f
Josh Kadis <josh@alleyinteractive.com>
2016-03-22 17:02:53 -0400

    update enqueues

--- php/class-fieldmanager-select.php
+++ php/class-fieldmanager-select.php
@@ -63,8 +63,8 @@ class Fieldmanager_Select extends Fieldmanager_Options {
 
 		// Add the chosen library for type-ahead capabilities
 		if ( $this->type_ahead ) {
-			fm_add_script( 'chosen', 'js/chosen/chosen.jquery.js' );
-			fm_add_style( 'chosen_css', 'js/chosen/chosen.css' );
+			fm_add_script( 'fm_chosen', 'js/chosen/chosen.jquery.js', array( 'jquery' ), '1.5.1' );
+			fm_add_style( 'fm_chosen_css', 'js/chosen/chosen.css', array(), '1.5.1' );
 		}
 
 	}
