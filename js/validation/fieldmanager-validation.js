f5753f8397f3993cd5064f61871df8e0e2e4a5f1
Shady Sharaf <shady@sharaf.me>
2016-08-31 17:09:07 +0200

    Spacing and CS tweaks

--- js/validation/fieldmanager-validation.js
+++ js/validation/fieldmanager-validation.js
@@ -1,39 +1,53 @@
 /*globals FM_VALIDATION_OPTIONS*/
-( function( $ ) {
+(function( $ ) {
 	'use strict';
 
 	var args = $.extend( {}, FM_VALIDATION_OPTIONS.options, {
-		ignore: function( index, form_element ) {
-			var $element = $( form_element );
+
+		ignore: function( index, formElement ) {
+
+			var $element = $( formElement );
+
+			// Field wrapper is visible, not behind a display_if
+			// And the field input exists in force option
 			if (
-				$element.closest( '.fm-item' ).is( ':visible' ) // Field wrapper is visible, not behind a display_if
-				&&
-				$element.is( FM_VALIDATION_OPTIONS.force.join( ', ' ) ) // And the field input exists in force option
+				$element.closest( '.fm-item' ).is( ':visible' ) &&
+				$element.is( FM_VALIDATION_OPTIONS.force.join( ', ' ) )
 			) {
+
 				// Force inclusion of fields added to FM_VALIDATION_OPTIONS.force
 				return false;
 			} else {
+
 				// Other inputs follow the rules in FM_VALIDATION_OPTIONS.ignore
 				return ! $element.is( FM_VALIDATION_OPTIONS.ignore.join( ', ' ) );
 			}
 		},
 		invalidHandler: function( event, validator ) {
+
 			// Certain WordPress forms require additional cleanup to work smoothly with jQuery validation
 			var form = $( validator.currentForm ).attr( 'id' );
+
 			switch ( form ) {
-				case "post":
-					$( "#submitpost .spinner" ).hide();
-					$( "#submitpost #publish" ).removeClass( 'button-primary-disabled' );
-					$(window).off( 'beforeunload.edit-post' );
+
+				case 'post':
+
+					$( '.spinner', '#submitpost' ).hide();
+					$( '#publish', '#submitpost' ).removeClass( 'button-primary-disabled' );
+					$( window ).off( 'beforeunload.edit-post' );
+
 					break;
 			}
 		},
-		submitHandler: function( form_element ) {
-			$(window).off( 'beforeunload.edit-post' );
-			form_element.submit();
+		submitHandler: function( formElement ) {
+
+			$( window ).off( 'beforeunload.edit-post' );
+			formElement.submit();
 		}
 	} );
 
-	$( 'form#' + FM_VALIDATION_OPTIONS.form_id ).validate( args );
+	if ( 'undefined' !== typeof FM_VALIDATION_OPTIONS.form_id ) {
+		$( 'form#' + FM_VALIDATION_OPTIONS.form_id ).validate( args );
+	}
 
-} )( jQuery );
+})( jQuery );
