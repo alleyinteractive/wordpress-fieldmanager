1f9ce824e28b99b38dd4e70a29a8fa6d18066ca6
David Herrera <david@alleyinteractive.com>
2016-11-22 23:53:13 -0500

    Avoid ternary for $autoload in `add_option()`
    Booleans are allowed for 'yes' and 'no' as of WordPress 4.2.

--- php/context/class-fieldmanager-context-submenu.php
+++ php/context/class-fieldmanager-context-submenu.php
@@ -157,7 +157,7 @@ class Fieldmanager_Context_Submenu extends Fieldmanager_Context_Storable {
 		if ( isset( $current ) ) {
 			update_option( $this->fm->name, $data, $this->wp_option_autoload );
 		} else {
-			add_option( $this->fm->name, $data, '', $this->wp_option_autoload ? 'yes' : 'no' );
+			add_option( $this->fm->name, $data, '', $this->wp_option_autoload );
 		}
 
 		return true;
