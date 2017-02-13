657eff8a5502f089937afafdb4d631969b9ad0f9
Kyle Benk <kyle@alleyinteractive.com>
2017-02-12 19:07:55 -0500

    Add test for no data and filtered data

--- tests/php/test-fieldmanager-rest-api.php
+++ tests/php/test-fieldmanager-rest-api.php
@@ -15,6 +15,7 @@ class Test_Fieldmanager_REST_API extends Fieldmanager_REST_API_Controller {
 	 */
 	function setup() {
 		$this->test_field = rand_str();
+		$this->new_test_data = rand_str();
 		$this->admin_id = $this->factory->user->create( array(
 			'role' => 'administrator',
 		) );
@@ -33,7 +34,7 @@ class Test_Fieldmanager_REST_API extends Fieldmanager_REST_API_Controller {
 	}
 
 	/**
-	 * Test out the post context.
+	 * Test the post context.
 	 */
 	function test_fieldmanager_rest_api_post() {
 		// Add actions for post context.
@@ -56,7 +57,54 @@ class Test_Fieldmanager_REST_API extends Fieldmanager_REST_API_Controller {
 	}
 
 	/**
-	 * Test out the term context.
+	 * Test the post context with a field that should not be in the response.
+	 */
+	function test_fieldmanager_no_rest_api_post() {
+		// Add actions for post context.
+		add_action( 'fm_post_posts', array( $this, '_fm_no_post_test_fields' ) );
+		add_action( 'fm_post_posts', array( $this, '_fm_no_post_test_fields' ) );
+
+		// Create the post.
+		$post_id = $this->factory->post->create();
+
+		// Add data.
+		$test_data = rand_str();
+		update_post_meta( $post_id, $this->test_field, $test_data );
+
+		// Process the REST API call.
+		$request = new WP_REST_Request( 'GET', '/wp/v2/posts/' . $post_id );
+		$response = $this->server->dispatch( $request );
+		$data = $response->get_data();
+
+		$this->assertArrayNotHasKey( $this->test_field, $data );
+	}
+
+	/**
+	 * Test the post context with a filter
+	 */
+	function test_fieldmanager_rest_api_post_filter() {
+		// Add actions for post context.
+		add_action( 'fm_post_posts', array( $this, '_fm_post_test_fields' ) );
+		add_action( 'fm_post_posts', array( $this, '_fm_post_test_fields' ) );
+		add_action( 'fm_rest_get', array( $this, '_fm_post_test_fields_filter' ), 10, 5 );
+
+		// Create the post.
+		$post_id = $this->factory->post->create();
+
+		// Add data.
+		$test_data = rand_str();
+		update_post_meta( $post_id, $this->test_field, $test_data );
+
+		// Process the REST API call.
+		$request = new WP_REST_Request( 'GET', '/wp/v2/posts/' . $post_id );
+		$response = $this->server->dispatch( $request );
+		$data = $response->get_data();
+
+		$this->assertEquals( $data[ $this->test_field ], $this->new_test_data );
+	}
+
+	/**
+	 * Test the term context.
 	 */
 	function test_fieldmanager_rest_api_term() {
 		// Add actions for term context.
@@ -79,7 +127,7 @@ class Test_Fieldmanager_REST_API extends Fieldmanager_REST_API_Controller {
 	}
 
 	/**
-	 * Test our the user context.
+	 * Test the user context.
 	 */
 	function test_fieldmanager_rest_api_user() {
 		// Add actions for user context.
@@ -110,6 +158,35 @@ class Test_Fieldmanager_REST_API extends Fieldmanager_REST_API_Controller {
 			'show_in_rest' => true,
 		] );
 		$fm_post->add_meta_box( __( 'Test Fields', 'fieldmanager' ), array( 'post' ), 'side' );
+	}
+
+	/**
+	 * Add field without adding it to the REST repsonse.
+	 */
+	function _fm_no_post_test_fields() {
+		$fm_post = new Fieldmanager_TextField( [
+			'name' => $this->test_field,
+			'show_in_rest' => false,
+		] );
+		$fm_post->add_meta_box( __( 'NO Test Fields', 'fieldmanager' ), array( 'post' ), 'side' );
+	}
+
+	/**
+	 * Filter the data returned.
+	 *
+	 * @param  mixed           $data        The current data returned from the REST API.
+	 * @param  array           $object      The REST API object.
+	 * @param  string          $field_name  The REST API field name.
+	 * @param  WP_REST_Request $request     The full request object from the REST API.
+	 * @param  string          $object_type The REST API object type.
+	 * @return mixed                        The data published to the REST API.
+	 */
+	function _fm_post_test_fields_filter( $data, $object, $field_name, $request, $object_type ) {
+		if ( $this->test_field === $field_name ) {
+			return $this->new_test_data;
+		}
+
+		return $data;
 	}
 
 	/**
