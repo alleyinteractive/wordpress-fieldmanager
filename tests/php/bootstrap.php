a83faa400c287a86d971938b87c5ab0c47e1b57a
Kyle Benk <kyle@alleyinteractive.com>
2017-02-13 13:49:10 -0500

    Hopefully fix Travis error

--- tests/php/bootstrap.php
+++ tests/php/bootstrap.php
@@ -11,8 +11,26 @@
  *
  */
 
-$_tests_dir = getenv('WP_TESTS_DIR');
-if ( ! $_tests_dir ) {
+/**
+ * Determine where the WP test suite lives.
+ *
+ * Support for:
+ * 1. `WP_DEVELOP_DIR` environment variable, which points to a checkout
+ *   of the develop.svn.wordpress.org repository (this is recommended)
+ * 2. `WP_TESTS_DIR` environment variable, which points to a checkout
+ * 3. `WP_ROOT_DIR` environment variable, which points to a checkout
+ * 4. Plugin installed inside of WordPress.org developer checkout
+ * 5. Tests checked out to /tmp
+ */
+if ( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
+	$_tests_dir = getenv( 'WP_DEVELOP_DIR' ) . '/tests/phpunit';
+} elseif ( false !== getenv( 'WP_TESTS_DIR' ) ) {
+	$_tests_dir = getenv( 'WP_TESTS_DIR' );
+} elseif ( false !== getenv( 'WP_ROOT_DIR' ) ) {
+	$_tests_dir = getenv( 'WP_ROOT_DIR' ) . '/tests/phpunit';
+} elseif ( file_exists( '../../../../tests/phpunit/includes/bootstrap.php' ) ) {
+	$_tests_dir = '../../../../tests/phpunit';
+} elseif ( file_exists( '/tmp/wordpress-tests-lib/includes/bootstrap.php' ) ) {
 	$_tests_dir = '/tmp/wordpress-tests-lib';
 }
 
