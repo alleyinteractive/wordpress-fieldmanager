7a853c3545a027f7eb0d6da56d81f1380e0029de
Kyle Benk <kyle@alleyinteractive.com>
2017-02-27 16:36:04 -0500

    Fix PHPCS

--- templates/options-radios.php
+++ templates/options-radios.php
@@ -1,4 +1,11 @@
-<?php $cb_id = $this->get_element_id() . '-' . esc_attr( sanitize_text_field( $data_row['value'] ) ); ?>
+<?php
+/**
+ * Template file for options radios.
+ *
+ * @package Templates
+ */
+
+$cb_id = $this->get_element_id() . '-' . esc_attr( sanitize_text_field( $data_row['value'] ) ); ?>
 <div class="fm-option">
 	<input
 		class="fm-element"
@@ -6,8 +13,8 @@
 		value="<?php echo esc_attr( $data_row['value'] ); ?>"
 		name="<?php echo esc_attr( $this->get_form_name() ); ?>"
 		id="<?php echo esc_attr( $cb_id ); ?>"
-		<?php echo $this->get_element_attributes(); ?>
-		<?php echo $this->option_selected( $data_row['value'], $value, "checked" ); ?>
+		<?php echo $this->get_element_attributes(); // Escaped in fuction call WPCS: XSS okay. ?>
+		<?php echo $this->option_selected( $data_row['value'], $value, 'checked' ); // Escaped in fuction call WPCS: XSS okay. ?>
 	/>
 	<label for="<?php echo esc_attr( $cb_id ); ?>" class="fm-option-label">
 		<?php echo esc_html( $data_row['name'] ); ?>
