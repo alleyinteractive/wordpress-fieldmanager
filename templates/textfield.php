292b883f717f01e5b6ad5e9bbc72f2c1a31d08ac
Kyle Benk <kyle@alleyinteractive.com>
2017-02-27 16:43:39 -0500

    Fix PHPCS

--- templates/textfield.php
+++ templates/textfield.php
@@ -1,8 +1,16 @@
+<?php
+/**
+ * Template file for a textfield
+ *
+ * @package Templates
+ */
+
+?>
 <input
 	class="fm-element"
-	type="<?php echo esc_attr( $this->input_type ) ?>"
+	type="<?php echo esc_attr( $this->input_type ); ?>"
 	name="<?php echo esc_attr( $this->get_form_name() ); ?>"
 	id="<?php echo esc_attr( $this->get_element_id() ); ?>"
 	value="<?php echo esc_attr( $value ); ?>"
-	<?php echo $this->get_element_attributes(); ?>
+	<?php echo $this->get_element_attributes(); // Escaped in function call WPCS XSS okay. ?>
 />
