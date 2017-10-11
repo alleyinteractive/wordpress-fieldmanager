4c7feafec148271594057a60aa1062f286516c3b
David Herrera <david@alleyinteractive.com>
2017-10-11 00:51:16 -0400

    Fix coding standards errors

--- templates/datepicker.php
+++ templates/datepicker.php
@@ -23,10 +23,12 @@
 	?>
 />
 
-<?php // Render only if no user-supplied options already exist. ?>
-<?php if ( empty( $this->js_opts['altField'] ) && empty( $this->js_opts['altFormat'] ) ) : ?>
-	<?php include fieldmanager_get_template( 'datepicker-altfield' ); ?>
-<?php endif; ?>
+<?php
+// Render only if no user-supplied options already exist.
+if ( empty( $this->js_opts['altField'] ) && empty( $this->js_opts['altFormat'] ) ) :
+	include fieldmanager_get_template( 'datepicker-altfield' );
+endif;
+?>
 
 <?php if ( $this->use_time ) : ?>
 	<span class="fm-datepicker-time-wrapper">
@@ -36,8 +38,8 @@
 		<input class="fm-element fm-datepicker-time" type="text" value="<?php echo esc_attr( $this->get_minute( $value ) ); ?>" name="<?php echo esc_attr( $this->get_form_name( '[minute]' ) ); ?>" />
 		<?php if ( $this->use_am_pm ) : ?>
 			<select class="fm-element" name="<?php echo esc_attr( $this->get_form_name( '[ampm]' ) ); ?>">
-				<option value="am"<?php selected( $this->get_am_pm( $value ), 'am' ) ?>><?php echo esc_html( $wp_locale->get_meridiem( 'AM' ) ); ?></option>
-				<option value="pm"<?php selected( $this->get_am_pm( $value ), 'pm' ) ?>><?php echo esc_html( $wp_locale->get_meridiem( 'PM' ) ); ?></option>
+				<option value="am"<?php selected( $this->get_am_pm( $value ), 'am' ); ?>><?php echo esc_html( $wp_locale->get_meridiem( 'AM' ) ); ?></option>
+				<option value="pm"<?php selected( $this->get_am_pm( $value ), 'pm' ); ?>><?php echo esc_html( $wp_locale->get_meridiem( 'PM' ) ); ?></option>
 			</select>
 		<?php endif; ?>
 	</span>
