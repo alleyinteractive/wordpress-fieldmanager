bb47758e5c1dd7ac58b3bcee778a0cf509b95c94
Erick Hitter <git-contrib@ethitter.com>
2018-03-30 11:30:35 -0700

    Extend Quickedit context to support bulk edit

--- js/fieldmanager-quickedit.js
+++ js/fieldmanager-quickedit.js
@@ -4,8 +4,8 @@
 		if ( typeof( inlineEditPost ) == 'undefined' ) {
 			return;
 		}
-		var wp_inline_edit = inlineEditPost.edit;
 
+		var wp_inline_edit = inlineEditPost.edit;
 		inlineEditPost.edit = function( id ) {
 			wp_inline_edit.apply( this, arguments );
 
@@ -28,7 +28,33 @@
 					} );
 				} );
 			}
-		}
+		};
+
+		var wp_inline_bulk_edit = inlineEditPost.setBulk;
+		inlineEditPost.setBulk = function() {
+			wp_inline_bulk_edit.apply( this );
+
+			$( '.fm-quickedit' ).each( function() {
+				var self = this;
+				var id = $( this ).attr( 'id' );
+				if ( id.substring( 0, 12 ) != 'fm-quickedit' ) {
+					return;
+				}
+				var column_name = id.substring( 13 );
+				$.get( ajaxurl, { action: 'fm_quickedit_render', 'column_name': column_name, 'post_id': 'bulk_edit', 'post_type': $( self ).data( 'fm-post-type' ) }, function( resp ) {
+					var temp = $( resp );
+					var dest = $( 'select', temp );
+
+					if ( dest ) {
+						var label = '&mdash; ' + window.fm_quickedit_opts.i18n.no_change + ' &mdash;';
+						var noChange = $('<option />').val( id ).html( label ).attr( 'selected', 'selected' );
+						dest.prepend(noChange);
+					}
+
+					$( self ).replaceWith( temp );
+				} );
+			} );
+		};
 	} );
 
 } )( jQuery );
