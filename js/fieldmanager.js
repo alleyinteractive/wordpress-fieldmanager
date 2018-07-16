24117c1ed1183ebb6ba021a2b827cc06eb47db77
Kevin Fodness <kevin@alley.co>
2018-07-16 15:41:06 -0400

    DIVE-131 Tabs, not spaces

--- js/fieldmanager.js
+++ js/fieldmanager.js
@@ -18,11 +18,11 @@ if ( ! Element.prototype.closest ) {
 		var el = this;
 		if ( ! document.documentElement.contains( el ) ) {
 			return null;
-    }
+		}
 		do {
 			if ( el.matches( s ) ) {
 				return el;
-      }
+			}
 			el = el.parentElement || el.parentNode;
 		} while ( el !== null && el.nodeType === 1 );
 		return null;
