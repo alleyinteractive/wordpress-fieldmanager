afe36a58d944eb644fd7fd4cc21d802dee1b1155
jomurgel <jo@alley.co>
2019-06-24 07:14:01 -0600

    changes options to attributes

--- templates/button.php
+++ templates/button.php
@@ -10,6 +10,6 @@
 	type="button"
 	name="<?php echo esc_attr( $this->get_form_name() ); ?>"
 	id="<?php echo esc_attr( $this->get_element_id() ); ?>"
-	value="<?php echo esc_attr( $this->options['button_text'] ); ?>"
+	value="<?php echo esc_attr( $this->attributes['button_text'] ); ?>"
 	<?php echo $this->get_element_attributes(); // Escaped interally. xss ok. ?>
 />
