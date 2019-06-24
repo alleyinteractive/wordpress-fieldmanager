ab4224b7281ac0e1493621f3079076c4ce60b0c6
jomurgel <jo@alley.co>
2019-06-24 13:07:16 -0600

    fixes phpcs errors

--- templates/button.php
+++ templates/button.php
@@ -6,16 +6,16 @@
  */
 
 // Add additional classnames, if they exist.
-$classnames = array_key_exists( 'class', $this->attributes ) ? $this->attributes['class'] : '';
+$fieldmanager_classnames = array_key_exists( 'class', $this->attributes ) ? $this->attributes['class'] : '';
 ?>
 
 <button
-	class="fm-button-element <?php echo esc_attr( $classnames ); ?>"
+	class="fm-button-element <?php echo esc_attr( $fieldmanager_classnames ); ?>"
 	id="<?php echo esc_attr( $this->get_element_id() ); ?>"
 	name="<?php echo esc_attr( $this->get_form_name() ); ?>"
 	role="<?php echo esc_attr( $this->input_type ); ?>"
 	type="<?php echo esc_attr( $this->input_type ); ?>"
 	<?php echo $this->get_element_attributes(); // Escaped interally. xss ok. ?>
 >
-	<?php echo $this->escape( 'button_content' ); // Escaped interally. xss ok.  ?>
+	<?php echo $this->escape( 'button_content' ); // Escaped interally. xss ok. ?>
 </button>
