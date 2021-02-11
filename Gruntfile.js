3649d64c95e81ff071d2349d838ba55214f7e0f4
Linnea Huxford <7308162+mslinnea@users.noreply.github.com>
2021-02-11 08:49:02 -0800

    Updates Gruntfile, npm dependencies, and builds new pot file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,44 +1,37 @@
 module.exports = function( grunt ) {
-	if ( ! grunt.option( 'wp' ) ) {
-		grunt.option( 'wp', 'master' );
-	}
 
-	grunt.initConfig({
-		connect: {
-			server: {
-				options: {
-					base: '.'
-				}
-			}
-		},
-		qunit: {
+	'use strict';
+
+	// Project configuration
+	grunt.initConfig( {
+
+		pkg: grunt.file.readJSON( 'package.json' ),
+
+		addtextdomain: {
 			options: {
-				timeout: 7000
+				textdomain: 'fieldmanager',
 			},
-			latest: {
+			update_all_domains: {
 				options: {
-					urls: ['http://localhost:8000/tests/js/index.html']
-				}
-			},
-			recent: {
-				options: {
-					urls: [
-						'http://localhost:8000/tests/js/index.html',
-						'http://localhost:8000/tests/js/index.html?wp=4.9',
-						'http://localhost:8000/tests/js/index.html?wp=4.8'
-					]
-				}
-			},
-			specific: {
-				options: {
-					urls: [ 'http://localhost:8000/tests/js/index.html?wp=' + grunt.option( 'wp' ) ]
-				}
+					updateDomains: true
+				},
+				src: [ '*.php', '**/*.php', '!\.git/**/*', '!bin/**/*', '!node_modules/**/*', '!tests/**/*' ]
 			}
 		},
+
+		wp_readme_to_markdown: {
+			your_target: {
+				files: {
+					'README.md': 'readme.txt'
+				}
+			},
+		},
+
 		makepot: {
 			target: {
 				options: {
 					domainPath: '/languages',
+					exclude: [ '\.git/*', 'bin/*', 'node_modules/*', 'tests/*' ],
 					mainFile: 'fieldmanager.php',
 					potFilename: 'fieldmanager.pot',
 					potHeaders: {
@@ -50,24 +43,14 @@ module.exports = function( grunt ) {
 				}
 			}
 		},
-		phpcs: {
-			plugin: {},
-			options: {
-				bin: "vendor/bin/phpcs",
-				showSniffCodes: true,
-				standard: "phpcs.ruleset.xml",
-				verbose: true,
-				warningSeverity: 0,
-			}
-		},
-	});
+	} );
 
-
-	grunt.loadNpmTasks( 'grunt-contrib-connect' );
-	grunt.loadNpmTasks( 'grunt-contrib-qunit' );
-	grunt.loadNpmTasks( 'grunt-phpcs' );
 	grunt.loadNpmTasks( 'grunt-wp-i18n' );
+	grunt.loadNpmTasks( 'grunt-wp-readme-to-markdown' );
+	grunt.registerTask( 'default', [ 'i18n','readme' ] );
+	grunt.registerTask( 'i18n', ['addtextdomain', 'makepot'] );
+	grunt.registerTask( 'readme', ['wp_readme_to_markdown'] );
 
-	// Run server for QUnit.
-	grunt.task.run( 'connect' );
+	grunt.util.linefeed = '\n';
+
 };
