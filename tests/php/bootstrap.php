7711291202c44c84d8de99f1f97aa0a7bbf37506
David Herrera <david@alley.co>
2021-02-25 22:25:27 -0500

    Use testing 'default' theme

--- tests/php/bootstrap.php
+++ tests/php/bootstrap.php
@@ -19,6 +19,9 @@ if ( ! $_tests_dir ) {
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
+	// Avoid interference from the default core theme.
+	switch_theme( 'default' );
+
 	require dirname( __FILE__ ) . '/../../fieldmanager.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
