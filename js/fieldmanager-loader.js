c25c3cacea224dddc1ed3f00b572144191232a21
Kyle Benk <kjbenk@gmail.com>
2021-11-29 20:20:02 -0500

    Refactor logic into a wrapped callback function

--- js/fieldmanager-loader.js
+++ js/fieldmanager-loader.js
@@ -5,43 +5,54 @@
  * @param {function} callback - The callback function to execute when the DOM is ready.
  */
  function fmLoadModule( callback ) {
-	// For the Gutenberg editor only, load FM module after metabox initialization.
-	if (wp.data && wp.blocks && wp.element) {
-		var metaboxesInitializedUnsubscribe = wp.data.subscribe(() => {
-			if (wp.data.select( 'core/edit-post' ).areMetaBoxesInitialized()) {
-				/**
-				 * `areMetaBoxesInitialized` is called immediately before the
-				 * `MetaBoxesArea` component is rendered, which is where the metabox
-				 * HTML is moved from the hidden div and into the main form element.
-				 *
-				 * This means we need to delay the callback a bit to allow this
-				 * component to render such that the jQuery `:visible` selector
-				 * can work as expected.
-				 */
-				setTimeout(() => {
-					callback();
-				}, 100);
+	/**
+	 * Wraps the provided callback to add check for the Gutenberg editor. If this
+	 * is called within the Gutenberg editor, then the callback function will trigger
+	 * after the metaboxes are initialized.
+	 */
+	const wrappedCallback = () => {
+		if (document.querySelector('.block-editor-page')) {
+			const metaboxesInitializedUnsubscribe = wp.data.subscribe(() => {
+				if ( wp.data.select( 'core/edit-post' ).areMetaBoxesInitialized() ) {
+					/**
+					 * `areMetaBoxesInitialized` is called immediately before the
+					 * `MetaBoxesArea` component is rendered, which is where the metabox
+					 * HTML is moved from the hidden div and into the main form element.
+					 *
+					 * This means we need to delay the callback a bit to allow this
+					 * component to render such that the jQuery `:visible` selector
+					 * can work as expected.
+					 *
+					 * @link https://github.com/WordPress/gutenberg/blob/019d0a1b1883a5c3e5c9cdecc60bd5e546b60a1b/packages/edit-post/src/components/meta-boxes/index.js#L38-L45
+					 * @link https://github.com/WordPress/gutenberg/blob/d39949a3b9dc8e12d5f5d33b9091f14b93b37c8a/packages/edit-post/src/components/meta-boxes/meta-boxes-area/index.js#L34-L36
+					 */
+					setTimeout(() => {
+						callback();
+					}, 100);
 
-				metaboxesInitializedUnsubscribe();
-			}
-		});
-	} else {
-		if ( 'object' === typeof wp && 'function' === typeof wp.domReady ) {
-			wp.domReady( callback );
-		} else if ( jQuery ) {
-			jQuery( document ).ready( callback );
-		} else {
-			// Shim wp.domReady.
-			if (
-				document.readyState === 'complete' || // DOMContentLoaded + Images/Styles/etc loaded, so we call directly.
-				document.readyState === 'interactive' // DOMContentLoaded fires at this point, so we call directly.
-			) {
-				callback();
-				return;
-			}
-
-			// DOMContentLoaded has not fired yet, delay callback until then.
-			document.addEventListener( 'DOMContentLoaded', callback );
+					metaboxesInitializedUnsubscribe();
+				}
+			});
+		} else{
+			callback();
 		}
+	};
+
+	if ( 'object' === typeof wp && 'function' === typeof wp.domReady ) {
+		wp.domReady( wrappedCallback );
+	} else if ( jQuery ) {
+		jQuery( document ).ready( wrappedCallback );
+	} else {
+		// Shim wp.domReady.
+		if (
+			document.readyState === 'complete' || // DOMContentLoaded + Images/Styles/etc loaded, so we call directly.
+			document.readyState === 'interactive' // DOMContentLoaded fires at this point, so we call directly.
+		) {
+			wrappedCallback();
+			return;
+		}
+
+		// DOMContentLoaded has not fired yet, delay callback until then.
+		document.addEventListener( 'DOMContentLoaded', wrappedCallback );
 	}
 }
