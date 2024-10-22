584da361cb232372d6d0abe2635901dbc420ec2e
Nathan Noom <nathan.noom@nbcuni.com>
2024-10-22 15:19:16 -0500

    feat(OTSD-54393): Update Module Reordering Interface in Config

--- php/class-fieldmanager-sortablelist.php
+++ php/class-fieldmanager-sortablelist.php
@@ -28,11 +28,8 @@ class Fieldmanager_SortableList extends Fieldmanager_Field {
 	public function __construct( $label = '', $options = array() ) {
 		parent::__construct( $label, $options );
 
-		// Enqueue necessary scripts and styles.
-		// Enqueue jQuery UI sortable.
 		wp_enqueue_script( 'jquery-ui-sortable' );
 
-		// Enqueue custom script to handle the sortable list.
 		fm_add_script( 'fm_sortablelist_js', 'js/fieldmanager-sortablelist.js', array( 'jquery', 'jquery-ui-sortable' ), FM_VERSION, true );
 		fm_add_style( 'fm_sortablelist_css', 'css/fieldmanager-sortablelist.css', array(), FM_VERSION );
 	}
@@ -44,29 +41,24 @@ class Fieldmanager_SortableList extends Fieldmanager_Field {
 	 * @return string The HTML output.
 	 */
 	public function form_element( $value ) {
-		// If no value is set, use the default order of items.
 		if ( empty( $value ) || ! is_array( $value ) ) {
 			$value = array_keys( $this->items );
 		}
-	
-		// Build the list of items to display
+
 		$items_to_display = array();
-	
-		// First, add items from $value that are still in $this->items
+
 		foreach ( $value as $item_key ) {
 			if ( isset( $this->items[ $item_key ] ) ) {
 				$items_to_display[] = $item_key;
 			}
 		}
-	
-		// Then, add any items from $this->items that are not already in $value
+
 		foreach ( $this->items as $item_key => $item_label ) {
 			if ( ! in_array( $item_key, $items_to_display ) ) {
 				$items_to_display[] = $item_key;
 			}
 		}
-	
-		// Build the list based on the current order.
+
 		$output = '<ul class="fm-sortable-list">';
 		foreach ( $items_to_display as $item_key ) {
 			$output .= sprintf(
@@ -76,8 +68,7 @@ class Fieldmanager_SortableList extends Fieldmanager_Field {
 			);
 		}
 		$output .= '</ul>';
-	
-		// Hidden input to store the order.
+
 		$output .= sprintf(
 			'<input type="hidden" name="%s" id="%s" value="%s" />',
 			esc_attr( $this->get_form_name() ),
@@ -95,9 +86,7 @@ class Fieldmanager_SortableList extends Fieldmanager_Field {
 	 * @return mixed The value to save.
 	 */
 	public function presave( $value, $current_values = array() ) {
-		// Save the order as an array of keys.
 		$order = explode( ',', $value );
-		// Validate the keys to ensure they exist in the items array.
 		$order = array_filter( $order, function( $key ) {
 			return array_key_exists( $key, $this->items );
 		} );
