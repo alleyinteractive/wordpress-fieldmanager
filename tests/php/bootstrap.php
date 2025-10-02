9a6c91ccdba986dd5cae84a46c0caa7d28fa208d
Sean Fisher <srtfisher@gmail.com>
2025-10-02 10:17:42 -0400

    Wrap up

--- tests/php/bootstrap.php
+++ tests/php/bootstrap.php
@@ -2,8 +2,7 @@
 /**
  * Bootstrap the testing environment
  *
- * Uses wordpress tests (http://github.com/nb/wordpress-tests/) which uses
- * PHPUnit.
+ * Uses {@see https://mantle.alley.com/testing/test-framework.html} for testing.
  *
  * Note: Do not change the name of this file. PHPUnit will automatically fire
  * this file when run.
@@ -19,20 +18,6 @@
 	} )
 	->install();
 
-// $_tests_dir = getenv( 'WP_TESTS_DIR' );
-// if ( ! $_tests_dir ) {
-// 	$_tests_dir = '/tmp/wordpress-tests-lib';
-// }
-
-// require_once $_tests_dir . '/includes/functions.php';
-
-// function _manually_load_plugin() {
-// 	require dirname( __FILE__ ) . '/../../fieldmanager.php';
-// }
-// tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
-
-// require $_tests_dir . '/includes/bootstrap.php';
-
 /**
  * Is the current version of WordPress at least ... ?
  *
@@ -43,7 +28,3 @@ function _fm_phpunit_is_wp_at_least( $min_version ) {
 	global $wp_version;
 	return floatval( $wp_version ) >= $min_version;
 }
-
-// // Load includes.
-// require_once __DIR__ . '/includes/class-fieldmanager-assets-unit-test-case.php';
-// require_once __DIR__ . '/includes/class-fieldmanager-options-mock.php';
